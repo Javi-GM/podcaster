658259a13bfad69a5b8a2bfa6b680c8c00fc3e10
Javier García <JaviGM>
2023-04-18 17:35:58 +0100

    test on red: should show 100 most popular podcasts

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,9 @@
 export const Home = () => {
     return (
-        <h1>Podcaster</h1>
+        <>
+            <h1>Podcaster</h1>
+            <section aria-label="Most popular podcasts on Apple Podcasts">
+            </section>
+        </>
     )
 }
--- src/test/pages/Home/Home.test.tsx
+++ src/test/pages/Home/Home.test.tsx
@@ -6,3 +6,13 @@ it("should render home page with the name of the app", () => {
     const title = screen.getByRole("heading", { name: "Podcaster", level: 1 });
     expect(title).toBeVisible();
 });
+
+it("should show 100 most popular podcast from Apple Podcasts", () => {
+    render(<Home />);
+    const podcastsSection = screen.getByRole(
+        "region",
+        { name: /Most popular podcasts on Apple Podcasts/i }
+    );
+    const podcasts = podcastsSection.querySelectorAll("article");
+    expect(podcasts).toHaveLength(100);
+});
